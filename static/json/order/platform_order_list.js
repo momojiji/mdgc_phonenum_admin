0449c743f6f8eb4c21d272eb8e91f1ec543150d5
madincwz <phperwz@163.com>
2020-05-07 14:16:49 +0800

    订单列表-平台

---
+++ static/json/order/platform_order_list.js
@@ -0,0 +1,116 @@
+{
+	"code": 0,
+	"msg": "",
+	"count": 30,
+	"data": [{
+		"id":"10001",
+		"order_id":"10000001",
+		"goods_id":"130000000",
+		"pay_money":"100",
+		"buyer":"杜甫",
+		"agent": "李白",
+		"supplier":"王维",
+		"order_status":"已完成",
+		"create_time":"2020-05-07 00:01:01"
+	},{
+		"id": "10002",
+		"order_id":"10000002",
+		"goods_id":"130000001",
+		"pay_money":"1001",
+		"buyer":"杜甫1",
+		"agent": "李白1",
+		"supplier":"王维1",
+		"order_status":"待支付",
+		"create_time":"2020-05-07 00:01:02"
+	},{
+		"id": "10002",
+		"order_id":"10000002",
+		"goods_id":"130000001",
+		"pay_money":"1001",
+		"buyer":"杜甫1",
+		"agent": "李白1",
+		"supplier":"王维1",
+		"order_status":"待支付",
+		"create_time":"2020-05-07 00:01:02"
+	},{
+		"id": "10002",
+		"order_id":"10000002",
+		"goods_id":"130000001",
+		"pay_money":"1001",
+		"buyer":"杜甫1",
+		"agent": "李白1",
+		"supplier":"王维1",
+		"order_status":"已取消",
+		"create_time":"2020-05-07 00:01:02"
+	},{
+		"id": "10002",
+		"order_id":"10000002",
+		"goods_id":"130000001",
+		"pay_money":"1001",
+		"buyer":"杜甫1",
+		"agent": "李白1",
+		"supplier":"王维1",
+		"order_status":"待支付",
+		"create_time":"2020-05-07 00:01:02"
+	},{
+		"id": "10002",
+		"order_id":"10000002",
+		"goods_id":"130000001",
+		"pay_money":"1001",
+		"buyer":"杜甫1",
+		"agent": "李白1",
+		"supplier":"王维1",
+		"order_status":"待支付",
+		"create_time":"2020-05-07 00:01:02"
+	},{
+		"id": "10002",
+		"order_id":"10000002",
+		"goods_id":"130000001",
+		"pay_money":"1001",
+		"buyer":"杜甫1",
+		"agent": "李白1",
+		"supplier":"王维1",
+		"order_status":"已完成",
+		"create_time":"2020-05-07 00:01:02"
+	},{
+		"id": "10002",
+		"order_id":"10000002",
+		"goods_id":"130000001",
+		"pay_money":"1001",
+		"buyer":"杜甫1",
+		"agent": "李白1",
+		"supplier":"王维1",
+		"order_status":"已完成",
+		"create_time":"2020-05-07 00:01:02"
+	},{
+		"id": "10002",
+		"order_id":"10000002",
+		"goods_id":"130000001",
+		"pay_money":"1001",
+		"buyer":"杜甫1",
+		"agent": "李白1",
+		"supplier":"王维1",
+		"order_status":"待支付",
+		"create_time":"2020-05-07 00:01:02"
+	},{
+		"id": "10002",
+		"order_id":"10000002",
+		"goods_id":"130000001",
+		"pay_money":"1001",
+		"buyer":"杜甫1",
+		"agent": "李白1",
+		"supplier":"王维1",
+		"order_status":"待支付",
+		"create_time":"2020-05-07 00:01:02"
+	},{
+		"id": "10002",
+		"order_id":"10000002",
+		"goods_id":"130000001",
+		"pay_money":"1001",
+		"buyer":"杜甫1",
+		"agent": "李白1",
+		"supplier":"王维1",
+		"order_status":"已完成",
+		"create_time":"2020-05-07 00:01:02"
+	}]
+}
